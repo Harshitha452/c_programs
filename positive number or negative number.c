dd15db778b1164796af0baa265b2fcaba8268fc4
Harshitha452 <harshithamajeti19@gmail.com>
2024-07-12 15:29:38 +0530

    Create positive number or negative number.c

---
+++ positive number or negative number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("a:");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("the positive number");
+    }
+    else
+    {
+        printf("the negative number");
+    }
+}
